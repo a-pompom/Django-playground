1b5d8229622c5bcc975552c4c771eee50df72a7b
a-pompom <43694794+a-pompom@users.noreply.github.com>
2022-04-22 12:58:57 +0900

    2022-04-22 バリデーション処理を実装
    テストコードのことを考えると、バリデーションの基本処理はvalidatorをカスタムメッセージで、固有の処理はユーザ定義validatorで書くのが良さそう

--- playground/simple_form/src/simple_form/forms/validation_form.py
+++ playground/simple_form/src/simple_form/forms/validation_form.py
@@ -1,8 +1,33 @@
 from django import forms
+from django.core import validators
+from django.core.exceptions import ValidationError
+
+
+def validate_password(value: str):
+    """
+    パスワードをバリデーション
+    
+    :param value: パスワードの入力値
+    """
+    # 擬似的にバリデーションエラーを送出するために不正なパスワードを仮で設定
+    invalid_password = 'password'
+
+    if value == invalid_password:
+        raise ValidationError('そのパスワードは使えません。')
 
 
 class ValidationForm(forms.Form):
     """ バリデーション機能を持ったformを表現することを責務に持つ """
-   
-    username = forms.CharField()
-    user_id = forms.CharField()
+
+    username = forms.CharField(
+        validators=[validators.RegexValidator(validators.slug_re, 'ユーザ名はSlug形式で入力してください。')],
+        error_messages={
+            'required': 'ユーザ名を入力してください。'
+        }
+    )
+    password = forms.CharField(
+        validators=[validate_password],
+        error_messages={
+            'required': 'パスワードを入力してください。'
+        }
+    )
